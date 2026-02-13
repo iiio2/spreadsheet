9fcdb442423dc2682b3fdf13353fe0bfcc3cc403
Mr. Robot <gomexxxx71@gmail.com>
2026-02-13 23:48:19 +0600

    chore: styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SpreadsheetProvider } from "@/context/SpreadsheetContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SpreadsheetProvider>
+      <Component {...pageProps} />
+    </SpreadsheetProvider>
+  );
 }
